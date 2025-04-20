19a494cb4fe3824416ed8ecb859e4f7ae6823d7a
Huang Li <t4rget.mail@gmail.com>
2025-04-20 12:52:31 -0600

    Update wrewrewjrwerjwer.el
    transparency china fix

--- wrewrewjrwerjwer.el
+++ wrewrewjrwerjwer.el
@@ -853,7 +853,7 @@ do
             BorderSizePixel = 0;
             Size = UDim2.new(0, 27, 0, 13);
             ZIndex = 5;
-            Image = 'rbxassetid://87506930188003';
+            Image = 'http://www.roblox.com/asset/?id=12977615774';
             Visible = not not Info.Transparency;
             Parent = DisplayFrame;
         });
